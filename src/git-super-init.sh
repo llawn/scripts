e01ed860939373633bc9e75776197b288a0b77cd
llawn <llawn06@gmail.com>
2025-02-13 19:06:37 +0100

    fix(git-super-init): github username was not well fetch

--- src/git-super-init.sh
+++ src/git-super-init.sh
@@ -7,25 +7,30 @@ if [ -z "$1" ]; then
 fi
 
 REPO_NAME=$1
-GITHUB_USERNAME=$(gh auth status 2>&1 | grep "Logged in as" | awk '{print $4}')
+GITHUB_USERNAME=$(gh api user | jq -r '.login')
+
+if [-z "$GITHUB_USERNAME" ]; then
+    echo "Error: Unable to retrieve Github username via gh auth status."
+    exit 1
+fi
+
+# Use ssh authentication
 GITHUB_REPO_URL="git@github.com:$GITHUB_USERNAME/$REPO_NAME.git"
 
-# Initialize the git repository locally
-git init
-
 # Create the repository on GitHub using GitHub CLI (gh)
-gh repo create "$REPO_NAME" --public --confirm
+gh repo create "$REPO_NAME" --public
+sleep 2
 
-# Add the remote to the local repository
+# Enter the cloned repository directory
+mkdir "$REPO_NAME"
+cd "$REPO_NAME" || exit
+
+# Add an initial README.md file and commit
+echo "# $REPO_NAME" > README.md
+git init
+git add README.md
+git commit -m "feat: initial commit"
 git remote add origin "$GITHUB_REPO_URL"
+git push -u origin main
 
-# Clone the repository (you can use 'git clone' directly or choose not to in this case)
-git clone "$GITHUB_REPO_URL"
-
-# Enter the repository directory and push the initial commit
-cd "$REPO_NAME"
-git add .
-git commit -m "Initial commit"
-git push -u origin master
-
-echo "Repository $REPO_NAME created and pushed to GitHub!"
+echo "Repository '$REPO_NAME' created, cloned and initialize"
